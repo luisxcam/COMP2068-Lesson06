caa6d0ca3f2d999f7c353134b3ebb5b2e68ce187
luisxcam <luisxcam@gmail.com>
2014-10-09 12:28:17 -0400

    Started typejs with easeljs

--- COMP2068-Lesson06/Scripts/_references.js
+++ COMP2068-Lesson06/Scripts/_references.js
@@ -1,2 +1,4 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="jquery-2.1.1.js" />
+/// <reference path="lib/easeljs-0.7.1.min.js" />
+/// <reference path="main.js" />
--- COMP2068-Lesson06/Scripts/main.js
+++ COMP2068-Lesson06/Scripts/main.js
@@ -3,7 +3,7 @@
 function init() {
     stage = new createjs.Stage(document.getElementById("canvas"));
     createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick",handleTick);
+    createjs.Ticker.addEventListener("tick", handleTick);
 }
 
 function handleTick() {
