b3e785473f9bc68ffed16f8a3dcd791e1861a361
luisxcam <luisxcam@gmail.com>
2014-10-09 12:50:17 -0400

    Experimentation with EaseJS

--- COMP2068-Lesson06/Scripts/main.js
+++ COMP2068-Lesson06/Scripts/main.js
@@ -1,13 +1,45 @@
 ﻿var stage;
+var myButton;
 
 function init() {
     stage = new createjs.Stage(document.getElementById("canvas"));
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", handleTick);
+
+    drawSlotMachine();
+    drawMyButton();
 }
 
 function handleTick() {
     stage.update();
+}
+
+function drawSlotMachine() {
+    var slotMachine = new createjs.Bitmap("img/slot_machine.png");
+    slotMachine.x = 0;
+    slotMachine.y = 0;
+
+    myButton = new createjs.Bitmap("img/logo.gif");
+    myButton.x = 100;
+    myButton.y = 100;
+
+    stage.addChild(slotMachine, myButton);
+
+    myButton.addEventListener("click", clickHandler);
+    myButton.addEventListener("mouseover", overHandler);
+    myButton.addEventListener("mouseout", outHandler);
+}
+
+function clickHandler() {
+    alert("you clicked me");
+}
+
+function overHandler() {
+    myButton.alpha = 0.5;
+}
+
+function outHandler() {
+    myButton.alpha = 1.0;
 }
 
 
---
+++ COMP2068-Lesson06/Scripts/type.js
@@ -0,0 +1,12 @@
+﻿var stage;
+
+function init() {
+    stage = new createjs.Stage(document.getElementById("canvas"));
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", handleTick);
+}
+
+function handleTick() {
+    stage.update();
+}
+//# sourceMappingURL=type.js.map
